49d5e06b517ad565dbe8e027e046bb3c3da22d59
davidreke <54542172+davidreke@users.noreply.github.com>
2020-01-23 12:03:38 -0500

    init upload

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+
+// for loop intro, console log 1-10
+
+
+
+//  play with for-loop, 
+// 1. adjust i in starting statement
+// 2. adjust in in ending statement
+// 3. adjust increment statement
+// 4. use a different variable
+
+
+
+
+// use for loop on an array
+
+// var interview = ["lucky tie", "suit", "Padfolio", "Resumes", "pen", "preperation"]
+
+
+// for(i=0; i<interview.length; i++)
+// {
+//     console.log(interview[i]);
+// }
+
+
+//  use a a for loop to do array math
+
+// var numbers = [1,2,5,7,8,12]
+
+// for(i=0; i<numbers.length; i++){
+//     numbers[i]= numbers[i]*2;
+// }
+
+// console.log(numbers)
+
+
+
+
